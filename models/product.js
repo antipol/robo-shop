e2ea8f57809ce0bcafab3a5d40690641601058c1
Benjamin-DCI <47890043+Benjamin-DCI@users.noreply.github.com>
2019-12-09 10:15:46 +0100

    Initial commit, boilerplate

---
+++ models/product.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const ProductSchema = new Schema({
+  imgUrl: { type: String, required: true },
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: String, required: true }
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
